a2d3bda48aace04c3258f5d9144701efe1c3666e
Chris Neal <chrisvneal@gmail.com>
2018-11-08 16:59:07 -1000

    Remove static data from component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import Search from './components/Search'
 import ListBooks from './components/ListBooks'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
+
   state = {
-    showSearchPage: false
+    books : []
   }
 
+  componentDidMount() {
+    BooksAPI.getAll().then(books => this.setState({books}))
+  }
   render() {
     return (
       <div className="app">
