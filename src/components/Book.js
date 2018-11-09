a6a2845aa686dbbec919b12243e3781f769f9a2b
Chris Neal <chrisvneal@gmail.com>
2018-11-08 18:32:05 -1000

    Minor refactor to book.title

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 
 class Book extends Component {
   render() {
+    let book = this.props.book;
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks.smallThumbnail}")` }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.smallThumbnail}")` }}></div>
           <div className="book-shelf-changer">
             <select>
               <option value="move" disabled>Move to...</option>
@@ -16,8 +17,8 @@ class Book extends Component {
             </select>
           </div>
         </div>
-        <div className="book-title">{this.props.book.title}</div>
-        <div className="book-authors">{this.props.book.authors}</div>
+        <div className="book-title">{book.title}{book.subtitle ? ': ' + book.subtitle : ''}</div>
+        <div className="book-authors">{book.authors}</div>
       </div>
     )
   }
